7ac4e33b9da80dbfcbe37657936168595279f7ec
MonABC <maitienvietdhcn@gmail.com>
2022-04-14 08:33:45 +0700

    crud group lien quan den 2 bang group va group_ member

---
+++ src/main/java/com/codegym/module4casestudy/service/group/IGroupService.java
@@ -0,0 +1,9 @@
+package com.codegym.module4casestudy.service.group;
+
+import com.codegym.module4casestudy.model.entity.Group;
+import com.codegym.module4casestudy.service.IGeneralService;
+
+
+public interface IGroupService extends IGeneralService<Group> {
+
+}
