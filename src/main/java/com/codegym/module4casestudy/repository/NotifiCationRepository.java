cdb165bfaea1277d83ca857804ba9f2e63d3ee2e
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-04-16 09:26:58 +0700

    them ban

--- src/main/java/com/codegym/module4casestudy/repository/NotifiCationRepository.java
+++ src/main/java/com/codegym/module4casestudy/repository/NotifiCationRepository.java
@@ -6,10 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NotifiCationRepository extends JpaRepository<NotificationAddFriends, Long> {
     @Query(value = "select * from notification_add_friends where to_user_id = ?1",nativeQuery = true)
     List<NotificationAddFriends>  showNotifications(Long toUserId);
 
+    @Query(value = "select * from notification_add_friends where from_user_id = ?1 and status = 1", nativeQuery = true)
+    List<NotificationAddFriends> listFriends(Long fromUserId);
+
+    @Query(value = "delete from notification_add_friends where status = 2",nativeQuery = true)
+    void deleteNotification();
+
+    @Query(value = "select * from notification_add_friends where from_user_id = ?1 and to_user_id = ?2", nativeQuery = true)
+    Optional<NotificationAddFriends> findNotificationAddFriends(Long fromUserId, Long toUserId);
 }
